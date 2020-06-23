bb52d0bf3b6c79460fd60aec4c5a05b2a9f30a34
MarckTomack <marcktomack@tutanota.com>
2020-06-23 20:56:05 +0200

    add version

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aloneMP/app"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/user"
@@ -11,9 +12,12 @@ import (
 
 // TODO: use cobra
 
+var version string
+
 var dir *string
 var server = flag.Bool("s", false, "Run the http server")
 var address = flag.String("addr", "127.0.0.1:3777", "http server address")
+var ver = flag.Bool("version", false, "version")
 
 func main() {
 	mainApp := app.NewApp()
@@ -23,19 +27,23 @@ func main() {
 	}
 	musicDir := filepath.Join(u.HomeDir, "Music")
 	dir = flag.String("dir", musicDir, "Directory with audio files")
-
 	flag.Parse()
 
-	f, err := os.Stat(*dir)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if f.IsDir() {
-		if *server {
-			mainApp.RunHttpServer(*address)
-		}
-		mainApp.Run(*dir)
+	if *ver {
+		fmt.Printf("\naloneMP version: %s\n\n", version)
 	} else {
-		log.Fatalf("%s is not a directory\n", *dir)
+		f, err := os.Stat(*dir)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if f.IsDir() {
+			if *server {
+				mainApp.RunHttpServer(*address)
+			}
+			mainApp.Run(*dir)
+		} else {
+			log.Fatalf("%s is not a directory\n", *dir)
+		}
 	}
+
 }
